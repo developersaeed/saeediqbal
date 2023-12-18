a7908977e3a64f2e2f5bb8b89a2bec219a00965e
Saeed Iqbal <76085688+saeediqbalofficial@users.noreply.github.com>
2023-12-18 09:03:14 +0500

    images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './imges/logo.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <h3>This website is under Maintenance</h3>
-          <p>Contact at : Saeediqbalofficial@gmail.com</p>
+        <img src={logo} />
+          <h4>This website is under Maintenance</h4>
+          <h6>Contact at : Saeediqbalofficial@gmail.com</h6>
             </header>
     </div>
   );
