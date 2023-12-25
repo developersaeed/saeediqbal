20d9b78f16117e399a5fa3cf550cbb92a1efa85f
Saeed Iqbal <76085688+saeediqbalofficial@users.noreply.github.com>
2023-12-25 17:41:43 +0500

    feature image

--- src/Components/MyCom.jsx
+++ src/Components/MyCom.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet';
-import  FeaturedImage  from 'featuredimage.png';
+import  FeaturedImage  from './featuredimage.png';
 
 const MyCom = () => {
   return (
